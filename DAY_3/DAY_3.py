9f4c9cc8a66c8cf6d4d559d51516c6185f0904b8
seafari <seafari46@gmail.com>
2025-09-09 19:29:29 +0530

    Add pizza bill calculator project

--- DAY_3/DAY_3.py
+++ DAY_3/DAY_3.py
@@ -76,3 +76,31 @@ if height >= 120:
     print(f"Your total bill is: {bill}")
 else:
     print("Sorry you have to grow taller before you can ride.")
+    
+    
+## Project - Pizza bill calculator
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+if size == "S":
+    bill = 15
+elif size == "M":
+    bill = 20
+else:
+    bill = 25
+    
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+        
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your total bill is: {bill}")
+
+
+
