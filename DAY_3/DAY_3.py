79909fd17679d0c0737ea24d0b662a88ae1115eb
seafari <seafari46@gmail.com>
2025-09-09 19:05:44 +0530

    Add conditional statements and modulo operator

---
+++ DAY_3/DAY_3.py
@@ -0,0 +1,21 @@
+## conditional statements
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("You can ride the rollercoaster")
+else:
+    print("You can't ride the rollercoaster")
+    
+    
+## Modulo operator --> gives remainder
+
+print(10 % 3) ## remainder = 1
+
+## Check if number is even or odd
+user_input = int(input("enter a number"))
+if user_input % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
+
